5cc160722c15ed7da82ba7b0f61aecea9a6c8260
Rose Cass <rose.cass12@gmail.com>
2018-05-22 11:36:09 -0400

    Added products post type and custom php templates in inhabitent theme.

--- plugins/inhabitent-functionality/lib/functions/post-types.php
+++ plugins/inhabitent-functionality/lib/functions/post-types.php
@@ -5,4 +5,59 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_post_type
  */
 
-// Add your custom post types here...
+// Register Product Custom Post Type
+function product_post_type() {
+
+	$labels = array(
+		'name'                  => 'Products',
+		'singular_name'         => 'Product',
+		'menu_name'             => 'Products',
+		'name_admin_bar'        => 'Products',
+		'archives'              => 'Product Archives',
+		'attributes'            => 'Product Attributes',
+		'parent_item_colon'     => 'Product Parent Item:',
+		'all_items'             => 'All Products',
+		'add_new_item'          => 'Add New Product',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Item',
+		'edit_item'             => 'Edit Item',
+		'update_item'           => 'Update Item',
+		'view_item'             => 'View Item',
+		'view_items'            => 'View Items',
+		'search_items'          => 'Search Item',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into item',
+		'uploaded_to_this_item' => 'Uploaded to this item',
+		'items_list'            => 'Items list',
+		'items_list_navigation' => 'Items list navigation',
+		'filter_items_list'     => 'Filter items list',
+	);
+	$args = array(
+		'label'                 => 'Product',
+		'description'           => 'Shop items',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'product_type' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-cart',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'products', $args );
+
+}
+add_action( 'init', 'product_post_type', 0 );
---
+++ themes/inhabitent-theme/template-parts/content-product.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template part for displaying single products.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+		<?php endif; ?>
+
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+  
+    <!-- Social Media Buttons -->
+
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
